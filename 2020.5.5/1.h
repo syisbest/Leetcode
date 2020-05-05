b8bf4ddfeca03446fa6746a0618c22522b12ea29
sheny <973785296@qq.com>
2020-05-05 16:40:19 +0800

    验证二叉搜索树

--- 2020.5.5/1.h
+++ 2020.5.5/1.h
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+//递归
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
@@ -16,5 +17,27 @@ public:
         if(root==nullptr)   return true;
         if(root->val>=max||root->val<=min)  return false;
         return isTrue(root->left,min,root->val)&&isTrue(root->right,root->val,max);
+    }
+};
+
+
+//迭代
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        stack<TreeNode*> temp;
+        long long lastVal=LONG_MIN;
+        while(!temp.empty()||root!=nullptr){
+            while(root!=nullptr){
+                temp.push(root);
+                root=root->left;
+            }
+            root=temp.top();
+            temp.pop();
+            if(root->val<=lastVal)    return false;
+            lastVal=root->val;
+            root=root->right;
+        }
+        return true;
     }
 };
